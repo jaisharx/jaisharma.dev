d0e391839b4a6aa56803fb329a982ac6ce7b1f9b
jai-sharma <sendtojsharma@gmail.com>
2021-07-25 10:10:58 +0530

    switching to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
+import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
 import NextHead from 'components/next-head';
 import content from 'lib/seo';
 import GloablCSS from 'styles/globals';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <NextHead title={content.title} desc={content.desc} />
