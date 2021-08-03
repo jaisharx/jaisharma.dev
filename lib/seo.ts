5df7f1a455d78a89a3212b718eb89a9ed5bccc55
jai-sharma <sendtojsharma@gmail.com>
2021-08-03 10:18:13 +0530

    small change

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 const content = {
-    title: 'React, Next.js, and Front-End expert - Jai Sharma',
+    title: 'React, Next.js, and Front-End Expert - Jai Sharma',
     desc: 'jaisharma.dev is my small corner of the web showcasing projects I’ve been working on. I\'m a fan of building modern and clean UI, with performance in mind.',
 };
 
