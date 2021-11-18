b1f5038e34706b1bfa7df85dca84aadf85e59fdb
Jai Sharma <sendtojsharma@gmail.com>
2021-11-18 13:02:47 +0530

    improving 3d shadow styles in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,23 +2,14 @@ import { Box, Link, Heading } from '@chakra-ui/react'
 import Container from './container'
 
 export default function Footer() {
+  const DepthShadow = "1px 1px 0px #161919, 2px 2px 0px #161919, 3px 3px 0px #161919, 4px 4px 0px #161919, 5px 5px 0px #161919, 6px 6px 0px #161919, 7px 7px 0px #161919, 8px 8px 0px #161919, 9px 9px 0px #161919, 10px 10px 0px #161919, 11px 11px 0px #161919, 12px 12px 0px #161919;"
+
   return (
     <Box as={Container} type="footer" my={{ base: 20, md: 80 }}>
       <Heading
-        fontSize={{ base: '24px', md: '62px', lg: '92px' }}
         color="white"
-        textShadow=" 1px 1px 0px #161919,
-                    2px 2px 0px #161919,
-                    3px 3px 0px #161919,
-                    4px 4px 0px #161919,
-                    5px 5px 0px #161919,
-                    6px 6px 0px #161919,
-                    7px 7px 0px #161919,
-                    8px 8px 0px #161919,
-                    9px 9px 0px #161919,
-                    10px 10px 0px #161919,
-                    11px 11px 0px #161919,
-                    12px 12px 0px #161919;"
+        fontSize={{ base: '26px', md: '62px', lg: '92px' }}
+        textShadow={{ base: 'none', md: DepthShadow }}
       >
         Like what you see? <br /> Reach out to me at <br />
         <Link
