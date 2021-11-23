a3a967d16596d3ad9ae97f71bfec1259b5bf5651
Jai Sharma <sendtojsharma@gmail.com>
2021-11-23 11:28:37 +0530

    switching the link to copy the email to clipboard

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,21 @@
-import { Box, Link, Heading } from '@chakra-ui/react'
+import { Box, Link, Heading, useClipboard, useToast } from '@chakra-ui/react'
 import Container from './container'
 
 export default function Footer() {
-  const DepthShadow = "1px 1px 0px #161919, 2px 2px 0px #161919, 3px 3px 0px #161919, 4px 4px 0px #161919, 5px 5px 0px #161919, 6px 6px 0px #161919, 7px 7px 0px #161919, 8px 8px 0px #161919, 9px 9px 0px #161919, 10px 10px 0px #161919, 11px 11px 0px #161919, 12px 12px 0px #161919;"
+  const DepthShadow =
+    '1px 1px 0px #161919, 2px 2px 0px #161919, 3px 3px 0px #161919, 4px 4px 0px #161919, 5px 5px 0px #161919, 6px 6px 0px #161919, 7px 7px 0px #161919, 8px 8px 0px #161919, 9px 9px 0px #161919, 10px 10px 0px #161919, 11px 11px 0px #161919, 12px 12px 0px #161919;'
+
+  const { onCopy } = useClipboard('sendtojsharma@gmail.com')
+  const toast = useToast()
+
+  const copyEmailToClipboard = () => {
+    onCopy() // copy the text
+    toast({
+      title: 'Email copied to the clipboard.',
+      status: 'success',
+      duration: 5000,
+    })
+  }
 
   return (
     <Box as={Container} type="footer" my={{ base: 20, md: 80 }}>
@@ -13,8 +26,8 @@ export default function Footer() {
       >
         Like what you see? <br /> Reach out to me at <br />
         <Link
+          onClick={copyEmailToClipboard}
           transition=".2s all ease"
-          href="mailto:sendtojsharma@gmail.com"
           _focus={{ boxShadow: 'none' }}
           _hover={{ borderBottom: '12px solid #2DFFC0' }}
         >
