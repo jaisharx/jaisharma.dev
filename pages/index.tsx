82f16b0189947367f2aebbef1652b6e5575c2585
Jai Sharma <sendtojsharma@gmail.com>
2022-02-08 22:17:02 +0530

    adding decorative border to the entire site

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,21 @@ import Footer from 'components/footer'
 import Header from 'components/header'
 import Works from 'components/works'
 import Projects from 'components/projects'
+import { Box } from '@chakra-ui/react'
 
 export default function Homepage() {
   return (
-    <>
+    <Box
+      borderWidth={{ base: '4px', md: '8px' }}
+      borderColor="#474a49"
+      borderStyle="solid"
+      borderBottom="none"
+      borderTop="none"
+    >
       <Header />
       <Works />
       <Projects />
       <Footer />
-    </>
+    </Box>
   )
 }
