c79246ec8644c374552af2757b2b37dbb069b2d5
Jai Sharma <sendtojsharma@gmail.com>
2022-07-30 13:03:38 +0530

    Adding next zero intl NPM package

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,6 +2,7 @@ import { Box } from '@chakra-ui/react'
 import Card from './card'
 import Container from './container'
 
+import nextZeroIntlSrc from 'public/static/next-zero-intl.png'
 import devtoImageSrc from 'public/static/devto.png'
 import nextjsImageSrc from 'public/static/nextjs.png'
 import designwizImageSrc from 'public/static/designwiz.png'
@@ -10,8 +11,18 @@ export default function Projects() {
   return (
     <Box as={Container} type="section">
       <Card
-        id="webapp"
         count="01"
+        title="Next Zero Intl"
+        imgSrc={nextZeroIntlSrc}
+        url="https://next-zero-intl-docs.vercel.app/"
+        githubUrl="https://github.com/jaisharx/next-zero-intl"
+      >
+        Next Zero Intl is a NPM package that does automatic translations to more
+        than 100 locales with the help of Google Translate.
+      </Card>
+      <Card
+        id="webapp"
+        count="02"
         title="Dev.to Clone"
         imgSrc={devtoImageSrc}
         url="https://dev-to-pied.vercel.app/"
@@ -22,7 +33,7 @@ export default function Projects() {
       </Card>
       <Card
         id="website"
-        count="02"
+        count="03"
         title="Next.js Website"
         imgSrc={nextjsImageSrc}
         url="https://nextjs-org-website.vercel.app/"
@@ -32,7 +43,7 @@ export default function Projects() {
         homepage. I've tried to get this as pixel-perfect as possible.
       </Card>
       <Card
-        count="03"
+        count="04"
         title="Designwiz"
         imgSrc={designwizImageSrc}
         url="https://designwiz.vercel.app/"
