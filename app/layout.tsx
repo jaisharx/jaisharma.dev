29aea658d96cd9e4dd5200df84e9d75a4b2c6b23
Jai Sharma <sendtojsharma@gmail.com>
2023-11-15 09:58:51 +0530

    Adding og data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import SEOData from 'seo.config.json'
 export const metadata = {
   title: SEOData.title,
   description: SEOData.desc,
+  openGraph: {
+    title: SEOData.title,
+    description: SEOData.desc,
+    images: ['http://jaisharma.dev/preview.png'],
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
